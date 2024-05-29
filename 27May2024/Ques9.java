d99c2c68ff4339331c134d2e313aabda98970873
bhavyagup2808 <guptabhavya2808@gmail.com>
2024-05-29 19:44:05 +0530

    Practice Question of 29 May 2024

--- 27May2024/Ques9.java
+++ 27May2024/Ques9.java
@@ -1,20 +1,20 @@
-import java.time.Duration;
 import java.time.LocalDate;
-import java.util.*;
+import java.time.temporal.ChronoUnit;
 class Employee{
     String name;
     int salary;
-    LocalDate joinigDate=LocalDate.now();
+    LocalDate joiningDate=LocalDate.now();
     Employee(String name,int salary,LocalDate joinDate)
     {
         this.name=name;
         this.salary=salary;
-        this.joinigDate=joinigDate;
+        this.joiningDate=joinDate;
     }
     void serviceDuration()
     {
         LocalDate dateNow =LocalDate.now();
-        long diff= Duration.between(dateNow - this.joinigDate);
+        long time = ChronoUnit.DAYS.between(this.joiningDate, dateNow);
+        System.out.println("The no of days of service is "+time);
     }
 }
 
@@ -23,7 +23,8 @@ public class Ques9 {
     // Write a Java program to create a class called "Employee" with a name, salary, 
     // and hire date attributes, and a method to calculate years of service.
     public static void main(String[] args) {
-        LocalDate d1=new ;
-        Employee e1=new Employee("Bhavya", 50000, null)
+        LocalDate d1= LocalDate.of(2024,01,20) ;
+        Employee e1=new Employee("Bhavya", 50000, d1);
+        e1.serviceDuration();
     }
 }
