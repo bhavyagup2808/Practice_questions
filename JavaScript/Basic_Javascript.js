1db43211d3703adc8be406118a442cd1944bc10b
bhavyagup2808 <guptabhavya2808@gmail.com>
2024-06-10 20:18:40 +0530

    Practice of javascript

--- JavaScript/Basic_Javascript.js
+++ JavaScript/Basic_Javascript.js
@@ -1,1 +1,22 @@
 // variable type
+const accountId=123456
+let accountEmail="guptabhavya2808@gmail.com"
+var accountPassword="12324"
+accountCity="Jaipur"
+
+console.log(accountId)
+console.table({accountEmail,accountId,accountPassword,accountCity})
+
+// ---------------------const------------------------------
+// These type of variable cannot be reassigned
+//const is block scope
+
+// ---------------------let----------------------------------
+// These type of variable can be reassigned
+//let is block scope
+
+//------------------------var--------------------------------
+// These type of variable can be reassigned
+//var is global scope
+
+
---
+++ JavaScript/Datatype_and_DatatypeConversion.js
@@ -0,0 +1,29 @@
+
+
+// ----------------------primitive--------------------
+//number=> 2to power 53
+//bigint
+//string
+//boolean =>true or false
+//null-type=>object
+//undefined -type =>undefined
+//symbol -type =>symbol
+
+//-------------------Non primitive-------------------
+//object
+//array
+
+//----------------------type conversion-----------------
+let score="33abc"
+let valueInNumber=Number(score)
+//output of typeof valueInNumber is NaN-> Not a number
+//score=null -> value=0;
+//score=undefined -> value=NaN
+
+//1->true 0->false
+//""->false "bcjhdcku"->true
+// "1"+2->"12"
+//"1"+2+2 ->"122"
+//1+2+"2"-> "32"
+
+
